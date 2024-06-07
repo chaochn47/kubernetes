4fde6a29e65427167198edcb895cad1417e63ae0
Chao Chen <chaochn@amazon.com>
2024-06-07 10:30:37 -0700

    query pod with field selector from apiserver watch cache
    Signed-off-by: Chao Chen <chaochn@amazon.com>

--- staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
+++ staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
@@ -22,9 +22,7 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -59,30 +57,15 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{
+		FieldSelector:   "status.podIP=10.0.141.200,status.phase=Running",
+		ResourceVersion: "0",
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("matched pods resourceVersion %s, %d pods\n", pods.GetResourceVersion(), len(pods.Items))
+	for i, po := range pods.Items {
+		fmt.Printf("%dth pod name is %s, in namespae %s \n", i+1, po.Name, po.Namespace)
 	}
 }
